ca8fc0fd67578596096ab2a7bd8b05d66b0bf3c4
Muniyandi Perumal <pmuniyandi@gmail.com>
2022-04-21 15:00:15 +0530

    range example

---
+++ range.py
@@ -0,0 +1,21 @@
+print(range(20))
+print(range(20, 30))
+print(range(20, 30, 2))
+
+print("Without start point and step")
+for value in range(20):
+    print(value)
+print("With start point and without step")
+for value in range(20, 30):
+    print(value)
+
+print("With start point and with step")
+for value in range(20, 30, 5):
+    print(value)
+
+'''
+for(int i=0;i<100;i++)
+
+for(int i =0;i<3;i++)
+    list[i]
+'''
